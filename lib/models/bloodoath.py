9f3e5725013912268cc6b8b02ef928f89343ef02
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-15 14:08:23 -0700

    allmove

--- lib/models/bloodoath.py
+++ lib/models/bloodoath.py
@@ -6,13 +6,19 @@
 
 
 class BloodOath:
-    all = []
+    
     def __init__( self, initiation_date, cult, follower ):
         self.initiation_date = initiation_date
         self.cult = cult
         self.follower = follower
         BloodOath.all.append( self )
 
+
+
+    all = []
+
+
+    
     @classmethod
     def first_oath( cls ):
         return cls.all[0].follower
