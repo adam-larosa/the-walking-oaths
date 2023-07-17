12e7bb4071edd74e17def2ea21673f2055d542ba
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-16 19:18:01 -0700

    variable naming

--- lib/models/bloodoath.py
+++ lib/models/bloodoath.py
@@ -7,10 +7,10 @@
 
 class BloodOath:
     all = []
-    def __init__( self, initiation_date, cult, follower ):
+    def __init__( self, initiation_date, cult_instance, follower_instance ):
         self.initiation_date = initiation_date
-        self.cult = cult
-        self.follower = follower
+        self.cult = cult_instance
+        self.follower = follower_instance
         BloodOath.all.append( self )
 
 
