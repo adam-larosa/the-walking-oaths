3d3033cf606c1a791949df01fdb19cb83b8c1361
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-16 21:54:18 -0700

    meow

--- lib/models/follower.py
+++ lib/models/follower.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, String
 if 'lib' in __name__ :                          # This if / else allows 
     from lib.walkingdev import Base, session    # the files to be used when
-else:                                           # scripts use these same
+else:                                           # other scripts use these same
     from walkingdev import Base, session        # files from different places
-from sqlalchemy.orm import relationship#, backref                                                
+from sqlalchemy.orm import relationship                                           
 from sqlalchemy.ext.associationproxy import association_proxy
 from .bloodoath import BloodOath
 
